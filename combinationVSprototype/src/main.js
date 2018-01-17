c6a5320a8ceb863c2fd1a4b89842a58f9f15ab46
asher_sun <asher_sun@163.com>
2018-01-17 09:11:58 +0800

    组合VS继承

---
+++ combinationVSprototype/src/main.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import {WelcomeDialog} from "./components/WelcomeDialog";
+import './css/main.css'
+
+ReactDOM.render(
+	<div>
+		<WelcomeDialog/>
+	</div>,
+	document.getElementById('root')
+)
